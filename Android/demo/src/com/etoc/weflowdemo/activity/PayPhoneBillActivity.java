4096d0bb621ad4dee08f9ca3b883eeaeaa39f23f
wangac <wangjiamingac@126.com>
2015-02-26 12:02:53 +0800

    充话费

---
+++ Android/demo/src/com/etoc/weflowdemo/activity/PayPhoneBillActivity.java
@@ -0,0 +1,19 @@
+package com.etoc.weflowdemo.activity;
+
+import android.os.Message;
+
+public class PayPhoneBillActivity extends TitleRootActivity {
+
+	@Override
+	public boolean handleMessage(Message msg) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int subContentViewId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
